aa139e0c55200cfa7a60c8e1719f209ad9fd6f85
Andrewmumblebee <aceharris@hotmail.co.uk>
2018-04-16 10:38:46 +0100

    Updated model and operations script, fixed up network

--- src/model.py
+++ src/model.py
@@ -40,48 +40,65 @@ class CGAN:
         return weight, bias
 
     def calcImageSize(self,dh,dw,stride):
-        return int(math.ceil(float(dh)/float(stride))),int(math.ceil(float(dw)/float(stride)))
+        return int(math.ceil(float(dh)/float(stride))), int(math.ceil(float(dw)/float(stride)))
 
     def loadModel(self, model_path=None):
         if model_path: self.saver.restore(self.sess, model_path)
 
-    def buildGenerator(self, z, label, reuse=False, isTraining=True):
+    def buildGenerator(self, z, y, reuse=False, isTraining=True):
+        gf_dim = 64
         dim_0_h, dim_0_w = self.imageSize[0], self.imageSize[1] # Building backwards convolutional layers
         dim_1_h, dim_1_w = self.calcImageSize(dim_0_h, dim_0_w, stride=2)
         dim_2_h, dim_2_w = self.calcImageSize(dim_1_h, dim_1_w, stride=2)
         dim_3_h, dim_3_w = self.calcImageSize(dim_2_h, dim_2_w, stride=2)
         dim_4_h, dim_4_w = self.calcImageSize(dim_3_h, dim_3_w, stride=2)
+        # dim_5_h, dim_5_w = self.calcImageSize(dim_4_h, dim_4_w, stride=2)
 
         with tf.variable_scope("Generator") as scope:
             if reuse: scope.reuse_variables()
 
-            yb = tf.reshape(label, [self.batch_size, 1, 1, self.labelSize])
+            yb = tf.reshape(y, [self.batch_size, 1, 1, self.labelSize])
             # l = tf.one_hot(label, self.labelSize, name="label_onehot")
-            h = tf.concat([z, label], axis=1, name="concat_z")
+            z = tf.concat([z, y], axis=1, name="concat_z")
 
-            # fc1
-            self.g_fc1_w, self.g_fc1_b = self._fc_variable([self.zdim + self.labelSize, 512 * dim_4_h * dim_4_w],name="fc1")
-            h0 = tf.matmul(h, self.g_fc1_w) + self.g_fc1_b
-            h0 = tf.nn.relu(h0)
-            #
-            h0 = tf.reshape(h0, (self.batch_size, dim_4_w, dim_4_h, 512))
-            h0 = conv_cond_concat(h0, yb)
+            self.g_fc1_w, self.g_fc1_b = self._fc_variable([100 + self.labelSize, gf_dim * 8 * dim_4_h * dim_4_w], name="fc1")
+            h = tf.matmul(z, self.g_fc1_w) + self.g_fc1_b
+            h = tf.nn.relu(h)
 
-            h0 = deconv2d(h0, output_shape=[self.batch_size, dim_3_h, dim_3_w, 256], name="deconv4")
+            # h0 = linear(z, 256 * dim_3_h * dim_3_w, 'g_h0_lin')
+            # h0 = batch_norm(h0, is_training=isTraining, scope="gNorm4")
+            # h0 = tf.nn.relu(h0)
+            # h0 = tf.reshape(h0, (self.batch_size, dim_3_h, dim_3_w, 256))
+
+            # h0 = linear(z, gf_dim * 8 * dim_4_h * dim_4_w, 'g_h1_line')
+            # h0 = tf.reshape(h0, [-1, dim_4_h, dim_4_w, gf_dim * 8])
+            # h0 = batch_norm(h0, is_training=isTraining, scope="gNorm6")
+            # h0 = tf.nn.relu(h0)
+            # #h0 = tf.concat([h0, y], axis=1, name="concat_a")
+            # h0 = conv_cond_concat(h0, yb)
+
+            h0 = tf.reshape(h, (self.batch_size, dim_4_h, dim_4_w, gf_dim * 8))
+
+            # h0 = deconv2d(h0, output_shape=[self.batch_size, dim_4_h, dim_4_w, 512], name="deconv5")
+            # h0 = batch_norm(h0, is_training=isTraining, scope="gNorm5")
+           #  h0 = conv_cond_concat(h0, yb)
+
+            # Deconv 1 -> 256x256
+            h0 = deconv2d(h0, output_shape=[self.batch_size, dim_3_h, dim_3_w, gf_dim * 4], name="deconv4")
             h0 = batch_norm(h0, is_training=isTraining, scope="gNorm4")
             # h0 = tf.nn.dropout(h0, 0.5)
             h0 = tf.nn.relu(h0)
             h0 = conv_cond_concat(h0, yb)
 
             # Deconv 1 -> 128x128
-            h1 = deconv2d(h0, output_shape=[self.batch_size, dim_2_h, dim_2_w, 128], name="deconv3")
+            h1 = deconv2d(h0, output_shape=[self.batch_size, dim_2_h, dim_2_w, gf_dim * 2], name="deconv3")
             h1 = batch_norm(h1, is_training=isTraining, scope="gNorm3")
             # h1 = tf.nn.dropout(h1, 0.5)
             h1 = tf.nn.relu(h1)
             h1 = conv_cond_concat(h1, yb)
 
             # Deconv 2 -> 64x64
-            h2 = deconv2d(h1, output_shape = [self.batch_size, dim_1_h, dim_1_w, 64], name="deconv2")
+            h2 = deconv2d(h1, output_shape = [self.batch_size, dim_1_h, dim_1_w, gf_dim * 1], name="deconv2")
             h2 = batch_norm(h2, is_training=isTraining, scope="gNorm2")
             # h2 = tf.nn.dropout(h2, 0.5)
             h2 = tf.nn.relu(h2)
@@ -91,63 +108,88 @@ class CGAN:
             h3 = deconv2d(h2, output_shape=[self.batch_size, dim_0_h, dim_0_w, CHANNELS], name= "deconv1")
 
             # Using tanh as per improved DCGAN paper.
-            y = tf.nn.sigmoid(h3)
+            y = tf.nn.sigmoid(h3, name="sprite")
 
         return y
 
-    def buildDiscriminator(self, y, label, reuse=False):
+    def buildDiscriminator(self, z, y, reuse=False):
         with tf.variable_scope("Discriminator") as scope:
             if reuse: scope.reuse_variables()
+            df_dim = 64
+
 
             # conditional layer
-            yb = tf.reshape(label, [self.batch_size, 1, 1, self.labelSize])
-            x = conv_cond_concat(y, yb)
+            yb = tf.reshape(y, [self.batch_size, 1, 1, self.labelSize])
+            z = conv_cond_concat(z, yb)
 
             # conv1 CHANNELS + self.labelSize
-            h0 = conv2d(x, output_shape=[5, 5, 4 + self.labelSize, 64], name="conv1")
+            h0 = conv2d(z, output_dim = 4 + self.labelSize, name="conv1")
             h0 = batch_norm(h0, is_training=self.isTraining, scope="dNorm1")
             h0 = lrelu(h0)
             h0 = conv_cond_concat(h0, yb)
 
             # conv2
-            h1 = conv2d(h0, output_shape=[5, 5, 64 + self.labelSize, 128], name="conv2")
+            # h1 = conv2d(h0, output_dim = df_dim * 2 + self.labelSize, name="conv2")
+            h1 = conv2d(h0, output_dim = df_dim * 2, name="conv2")
             h1 = batch_norm(h1, is_training=self.isTraining, scope="dNorm2")
             h1 = lrelu(h1)
+            #n_b, n_h, n_w, n_f = [int(x) for x in h1.get_shape()]
+            #h1 = tf.reshape(h1, [self.batch_size, n_h * n_w * n_f])
+            #h1 = tf.reshape(h1, [self.batch_size, -1])
+            #h1 = tf.concat([h1, y], 1)
             h1 = conv_cond_concat(h1, yb)
 
             # conv3
-            h2 = conv2d(h1, output_shape=[5, 5, 128 + self.labelSize, 256], name="conv3")
+            h2 = conv2d(h1, output_dim= df_dim * 4, name="conv3")
+            # h2 = linear(h1, 128 + self.labelSize, 'd_h2_lin')
             h2 = batch_norm(h2, is_training=self.isTraining, scope="dNorm3")
             h2 = lrelu(h2)
+            #h2 = tf.concat([h1, y], 1)
             h2 = conv_cond_concat(h2, yb)
 
-            # conv4
-            h3 = conv2d(h2, output_shape=[5, 5, 256 + self.labelSize, 512], name="conv4")
+            # # conv4
+            h3 = conv2d(h2, output_dim=df_dim * 8, name="conv4")
             h3 = batch_norm(h3, is_training=self.isTraining, scope="dNorm4")
             h3 = lrelu(h3)
             h3 = conv_cond_concat(h3, yb)
 
+            # # conv5
+            # h4 = conv2d(h3, output_dim=[5, 5, 512 + self.labelSize, 1024], name="conv5")
+            # h4 = batch_norm(h4, is_training=self.isTraining, scope="dNorm5")
+            # h4 = lrelu(h4)
+            # h4 = conv_cond_concat(h4, yb)
+
+            # h3 = linear(tf.reshape(h2, [self.batch_size, -1]), 1, 'd_h4_lin')
+            # h4 = tf.reshape(h3, [self.batch_size, -1])
+            # h4 = linear(h4, 1, 'd_h3_lin')
+
+
             # fc1
             n_b, n_h, n_w, n_f = [int(x) for x in h3.get_shape()]
-            h3 = tf.reshape(h3,[self.batch_size,n_h*n_w*n_f])
-            self.d_fc1_w, self.d_fc1_b = self._fc_variable([n_h*n_w*n_f,1],name="fc1")
-            h3 = tf.matmul(h3, self.d_fc1_w) + self.d_fc1_b
+            h4 = tf.reshape(h3,[self.batch_size, n_h * n_w * n_f])
+            self.d_fc1_w, self.d_fc1_b = self._fc_variable([n_h * n_w * n_f, 1],name="fc1")
+            h4 = tf.matmul(h4, self.d_fc1_w) + self.d_fc1_b
 
-        return h3
+            # h4 = tf.nn.sigmoid(h4)
+
+        return h4
 
     def buildModel(self):
         # define variables
-        self.z      = tf.placeholder(tf.float32, [self.batch_size, self.zdim], name="z")
-        self.l      = tf.placeholder(tf.float32  , [self.batch_size, self.labelSize], name="label")
+        self.z = tf.placeholder(tf.float32, [self.batch_size, self.zdim], name="z")
+        self.l = tf.placeholder(tf.float32, [self.batch_size, self.labelSize], name="label")
 
         img_dimensions = [self.imageSize[0], self.imageSize[1], CHANNELS]
         self.g_real = tf.placeholder(tf.float32, [self.batch_size] + img_dimensions, name="images")
 
+
+        ### GENERATORS ###
         self.g_fake = self.buildGenerator(self.z, self.l)
         self.g_sample = self.buildGenerator(self.z, self.l, reuse=True, isTraining=False)
 
-        self.d_real  = self.buildDiscriminator(self.g_real, self.l)
-        self.d_fake  = self.buildDiscriminator(self.g_fake, self.l, reuse=True)
+        ### DISCRIMINATORS ###
+        self.d_real = self.buildDiscriminator(self.g_real, self.l)
+        self.d_fake = self.buildDiscriminator(self.g_fake, self.l, reuse=True)
 
         print("BUILT MODELS")
 
@@ -160,8 +202,8 @@ class CGAN:
         print("DEFINED LOSS FUNCTIONS")
 
         # define optimizer
-        self.g_optimizer = tf.train.AdamOptimizer(self.learning_rate,beta1=0.5).minimize(self.g_loss, var_list=[x for x in tf.trainable_variables() if "Generator"     in x.name])
-        self.d_optimizer = tf.train.AdamOptimizer(self.learning_rate,beta1=0.5).minimize(self.d_loss, var_list=[x for x in tf.trainable_variables() if "Discriminator" in x.name])
+        self.g_optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=0.5).minimize(self.g_loss, var_list=[x for x in tf.trainable_variables() if "Generator"     in x.name])
+        self.d_optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=0.5).minimize(self.d_loss, var_list=[x for x in tf.trainable_variables() if "Discriminator" in x.name])
 
         print("DEFINED OPTIMIZERS")
 
@@ -173,7 +215,7 @@ class CGAN:
 
         return
 
-    def train(self,f_batch):
+    def train(self, f_batch):
         def tileImage(imgs, size):
             d = int(math.sqrt(imgs.shape[0]-1))+1
             h, w = imgs.shape[1], imgs.shape[2]
@@ -184,7 +226,7 @@ class CGAN:
                     i = idx // d
                     j = idx-i*d
                     img[j * h:j * h + h, i * w:i * w + w, :] = image
-                return img
+                return img * 255. #  + 1) * 2)
             else:
                 raise ValueError('in merge(images,size) images parameter '
                                 'must have dimensions: HxW or HxWx3 or HxWx4')
@@ -204,7 +246,6 @@ class CGAN:
             batch_images, batch_labels = f_batch(self.batch_size)
             # Random noise vector
             batch_z                   = np.random.uniform(-1., +1., [self.batch_size, self.zdim])
-
             # Update generator
             _, g_loss                = self.sess.run([self.g_optimizer, self.g_loss],feed_dict={self.z: batch_z, self.l: batch_labels})
             _, g_loss                = self.sess.run([self.g_optimizer, self.g_loss],feed_dict={self.z: batch_z, self.l: batch_labels})
@@ -216,8 +257,12 @@ class CGAN:
                 print ("{}: loss(D)={:.4f}, loss(G)={:.4f}; time/step = {:.2f} sec".format(step, d_loss, g_loss, time.time() - start))
                 start = time.time()
 
-                l0 = np.array([[x%2, x%7, x%7, x%13, x%13, x%13, x%13] for x in range(self.batch_size)])
-                l1 = np.array([[0, x%9, x%12, x%20, x%20, x%20, x%30] for x in range(self.batch_size)])
+                l0 = np.random.uniform(-1, +1, [self.batch_size, self.labelSize])
+                #l1 = np.random.uniform(-1, +1, [self.batch_size, self.labelSize])
+                l1 = np.array([np.random.binomial(1, 0.5, self.labelSize) for x in range(self.batch_size)])
+                #l1 = np.array([[x % 2, x % 7, x % 12, x % 20, x % 20, x % 20, x % 30] for x in range(self.batch_size)])
+                # l1 = np.array([[x % 2, x % 8] for x in range(self.batch_size)])
+                #l1 = np.array([[0%2, 0%7] for x in range(self.batch_size)])
                 z1 = np.random.uniform(-1, +1, [self.batch_size, self.zdim])
                 z2 = np.random.uniform(-1, +1, [self.zdim])
                 z2 = np.expand_dims(z2, axis=0)
@@ -225,7 +270,8 @@ class CGAN:
 
                 g_image1 = self.sess.run(self.g_sample, feed_dict={self.z:z1, self.l:l0})
                 g_image2 = self.sess.run(self.g_sample, feed_dict={self.z:z2, self.l:l1})
-                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_real.png".format(step)), tileImage(g_real, [64, 64]) * 255.)
-                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_fake1.png".format(step)), tileImage(g_image1, [64, 64]) * 255.)
-                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_fake2.png".format(step)), tileImage(g_image2, [64, 64]) * 255.)
-                self.saver.save(self.sess,os.path.join(self.save_folder,"model.ckpt"),step)
+                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_real.png".format(step)), tileImage(g_real, [64, 64]))
+                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_fake1.png".format(step)), tileImage(g_image1, [64, 64]))
+                scipy.misc.imsave(os.path.join(self.save_folder,"images","img_{}d_fake2.png".format(step)), tileImage(g_image2, [64, 64]))
+                self.saver.save(self.sess,os.path.join(self.save_folder, "model.ckpt"), step)
+                # freeze_graph('Generator/sprite')
--- src/operations.py
+++ src/operations.py
@@ -38,15 +38,16 @@ def conv_cond_concat(x, y):
 def lrelu(x, leak=0.2, name="lrelu"):
     return tf.maximum(x, leak*x)
 
-def conv2d(input_, output_shape,
+def conv2d(input_, output_dim,
        filt=[5, 5], strides=[1, 2, 2, 1], stddev=0.02,
        name="conv2d"):
   with tf.variable_scope(name):
-    w = tf.get_variable('w', output_shape,
+    w = tf.get_variable('w', [filt[0], filt[1], input_.get_shape()[-1], output_dim],
               initializer=tf.truncated_normal_initializer(stddev=stddev))
+    print(input_.get_shape()[-1])
     conv = tf.nn.conv2d(input_, w, strides=strides, padding='SAME')
 
-    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
     conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
 
     return conv
@@ -63,3 +64,70 @@ def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=
             return tf.matmul(input_, matrix) + bias, matrix, bias
         else:
             return tf.matmul(input_, matrix) + bias
+
+def freeze_graph(output_node_names):
+    """Extract the sub graph defined by the output nodes and convert
+    all its variables into constant
+    Args:
+        model_dir: the root folder containing the checkpoint state file
+        output_node_names: a string, containing all the output node's names,
+                            comma separated
+    """
+    model_dir = r'C:\Users\andrew\Documents\Root\Repos\CC\AAA\src\models'
+    if not tf.gfile.Exists(model_dir):
+        raise AssertionError(
+            "Export directory doesn't exists. Please specify an export "
+            "directory: %s" % model_dir)
+
+    if not output_node_names:
+        print("You need to supply the name of a node to --output_node_names.")
+        return -1
+
+    # We retrieve our checkpoint fullpath
+    checkpoint = tf.train.latest_checkpoint(model_dir)
+
+    # We precise the file fullname of our freezed graph
+    absolute_model_dir = "\\".join(checkpoint.split('\\')[:-1])
+    output_graph = absolute_model_dir + "\\model.pb"
+
+    # We clear devices to allow TensorFlow to control on which device it will load operations
+    clear_devices = True
+
+    # We start a session using a temporary fresh Graph
+    with tf.Session(graph=tf.Graph()) as sess:
+        # We import the meta graph in the current default Graph
+        saver = tf.train.import_meta_graph(checkpoint + '.meta', clear_devices=clear_devices)
+
+        # We restore the weights
+        saver.restore(sess, checkpoint)
+
+        # get graph definition
+        gd = sess.graph.as_graph_def()
+
+        # fix batch norm nodes (Currently break when trying to save as constants.)
+        for node in gd.node:
+            if node.op == 'RefSwitch':
+                node.op = 'Switch'
+                for index in range(len(node.input)):
+                    if 'moving_' in node.input[index]:
+                        node.input[index] = node.input[index] + '/read'
+            elif node.op == 'AssignSub':
+                node.op = 'Sub'
+                if 'use_locking' in node.attr: del node.attr['use_locking']
+
+        # We use a built-in TF helper to export variables to constants
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess, # The session is used to retrieve the weights
+            gd, # The graph_def is used to retrieve the nodes
+            output_node_names.split(",") # The output node names are used to select the usefull nodes
+        )
+
+        # Finally we serialize and dump the output graph to the filesystem
+        with tf.gfile.GFile(output_graph, "wb") as f:
+            f.write(output_graph_def.SerializeToString())
+
+        print("Graph saved. {}".format(output_graph))
+
+        # print("%d ops in the final graph." % len(output_graph_def.node))
+
+    return output_graph_def
