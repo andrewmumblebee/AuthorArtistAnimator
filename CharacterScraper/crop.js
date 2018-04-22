36dec3d94e346ef0ad0c590177902a6447292935
Andrewmumblebee <aceharris@hotmail.co.uk>
2018-04-22 10:50:28 +0100

    Updated crop method, doing bulk crops now

--- CharacterScraper/crop.js
+++ CharacterScraper/crop.js
@@ -4,20 +4,31 @@ const charFolder = './dump/sheets';
 
 function cropimage(folder, file) {
     jimp.read(`${folder}/${file}`, function (err, img) {
-      img.crop(0, 128, 64, 64)
+      img.crop(0, 64, 64, 192)
       .write(`./dump/sprites/${file}`);
     });
 }
 var files = fs.readdirSync(charFolder);
 (async (files) => {
+  let promises = [];
   for (let i = 0; i <= files.length; i++) {
-    await new Promise((resolve) => {
+    promise = new Promise((resolve) => {
       jimp.read(`${charFolder}/${files[i]}`, function (err, img) {
-        img.crop(0, 128, 64, 64)
-        .write(`./dump/sprites/${files[i]}`);
+        img.clone().crop(0, 64, 64, 64)
+        .write(`./dump/sprites/f0_${files[i]}`);
+        img.clone().crop(0, 128, 64, 64)
+        .write(`./dump/sprites/f1_${files[i]}`);
+        img.crop(0, 256, 64, 64)
+        .write(`./dump/sprites/f2_${files[i]}`);
         resolve();
       });
     });
+    promises.push(promise);
+
+    if (promises.length >= 5) {
+      await Promise.all(promises);
+      promises = [];
+    }
   }
 })(files)
 
