6508e6bba8201be3f81af83fd4447636e6d4ed1b
Andrewmumblebee <aceharris@hotmail.co.uk>
2018-04-23 13:19:50 +0100

    Updated training script to allow training of animator

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import argparse
-from model import BatchGenerator, Artist
+from utility import BatchGenerator, ABatchGenerator
+from model import Artist, Animator
 import tensorflow as tf
 
 if __name__=="__main__":
@@ -13,7 +14,7 @@ if __name__=="__main__":
     flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
     flags.DEFINE_string("reload", None, "reload")
     flags.DEFINE_string("save_folder", "models", "Directory name to save the checkpoints [checkpoint]")
-    flags.DEFINE_string("dataset_folder", r'C:\Users\andrew\Documents\Root\Repos\CC\AAA\CharacterScraper\dump\large_sheets\sprites', "folder with images")
+    flags.DEFINE_string("dataset_folder", r"C:\Users\andrew\Documents\Root\Repos\CC\AAA\src\data", "folder with images")
     flags.DEFINE_boolean("train", True, "True for training, False for testing [True]")
     flags.DEFINE_string("model", 'Artist', "Model to train Artist, Author [Artist]")
     flags.DEFINE_integer("epoch", 200, "Number of epochs to train for [25]")
@@ -22,11 +23,16 @@ if __name__=="__main__":
     config = tf.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = 0.7
 
-    if FLAGS.model == "animator":
-        print("TO BE ADDED")
-    else:
-        with tf.Session(config=config) as sess:
-            batch = BatchGenerator(FLAGS.dataset_folder)
+    with tf.Session(config=config) as sess:
+        if FLAGS.model == "animator":
+            print("\nTraining Animator Network")
+            batch = ABatchGenerator(FLAGS.dataset_folder + r'\animations')
+            label_size = batch.get_label_size()
+            animator = Animator(sess=sess, isTraining=True, imageSize=[FLAGS.image_height, FLAGS.image_width], labelSize=label_size, args=FLAGS)
+            animator.train(batch_generator=batch)
+        else:
+            print("\nTraining Artist Network")
+            batch = BatchGenerator(FLAGS.dataset_folder + r'\sprites')
             label_size = batch.get_label_size()
             artist = Artist(sess=sess, isTraining=True, imageSize=[FLAGS.image_height, FLAGS.image_width], labelSize=label_size, args=FLAGS)
             artist.train(batch_generator=batch)
