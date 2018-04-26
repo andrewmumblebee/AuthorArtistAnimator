63c4b661db2bbf35a74711fcb30834fd6fa2d9b4
Andrewmumblebee <aceharris@hotmail.co.uk>
2018-04-26 16:58:58 +0100

    Added some comments and cleaned up operations a bit, supporting dynam batch size

--- src/operations.py
+++ src/operations.py
@@ -11,7 +11,19 @@ def batch_norm(x, is_training=True, scope="batch_norm", epsilon=1e-5, decay=0.9)
 
 def deconv2d(input_, output_shape,
        filt = [5, 5], strides=[1, 2, 2, 1], stddev=0.02,
-       name="deconv2d", with_w=False):
+       name="deconv2d"):
+    """ Performs a deconvolution on the input shape, based on supplied parameters.
+
+        Just a transpose of a convolution operation.
+
+        Args:
+            - input_: Input shape to have a deconvolution operation applied to.
+            - output_dim: Dimensions of the output shape created by the deconvolution.
+            - filt: Dimensions of feature maps to be created. [5,5]
+            - strides: Size of stride to take by (x,y) during deconvolution. [2, 2]
+            - stddev: might be removed.
+            - name: unique name to give node on graph.
+    """
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
         w = tf.get_variable('w', [filt[0], filt[1], output_shape[-1], input_.get_shape()[-1]],
@@ -22,56 +34,67 @@ def deconv2d(input_, output_shape,
                     strides=strides, padding = "SAME", data_format="NHWC")
 
         b = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-        deconv = tf.reshape(tf.nn.bias_add(deconv, b), deconv.get_shape())
+        deconv = tf.reshape(tf.nn.bias_add(deconv, b), tf.shape(deconv))
 
         tf.summary.histogram("{}_w".format(name), w)
         tf.summary.histogram("{}_b".format(name), b)
 
-        if with_w:
-            return deconv, w, b
-        else:
-            return deconv
+        return deconv
 
 def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
-  x_shapes = x.get_shape()
-  y_shapes = y.get_shape() # Conditioning vector with conditional properties
-  return tf.concat([x, y * tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+  x_shapes = tf.shape(x)
+  y_shapes = tf.shape(y)
+  y_ = y * tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])
+  return tf.concat([x, y_], 3)
 
 def lrelu(x, leak=0.2, name="lrelu"):
-    return tf.maximum(x, leak*x)
+    return tf.maximum(x, leak * x)
 
-def conv2d(input_, output_dim,
-       filt=[5, 5], strides=[1, 2, 2, 1], stddev=0.02,
-       name="conv2d"):
+def conv2d(input_, output_dim, filt=[5, 5], strides=[2, 2],
+           stddev=0.02, name="conv2d"):
+  """ Performs a convolution on the input shape, based on supplied parameters.
+
+      Args:
+        - input_: Input shape to have a convolution operation applied to.
+        - output_dim: Dimensions of the output shape created by the convolution.
+        - filt: Dimensions of feature maps to be created. [5,5]
+        - strides: Size of stride to take by (x,y) during convolution. [2, 2]
+        - stddev: might be removed.
+        - name: unique name to give node on graph.
+  """
   with tf.variable_scope(name):
     w = tf.get_variable('w', [filt[0], filt[1], input_.get_shape()[-1], output_dim], #tf.truncated_normal_initializer(stddev=stddev)
               initializer=tf.contrib.layers.xavier_initializer_conv2d())
-    conv = tf.nn.conv2d(input_, w, strides=strides, padding='SAME')
+    conv = tf.nn.conv2d(input_, w, strides=[1, strides[0], strides[1], 1], padding='SAME')
 
     b = tf.get_variable('b', [output_dim], initializer=tf.constant_initializer(0.0))
-    conv = tf.reshape(tf.nn.bias_add(conv, b), conv.get_shape())
+    conv = tf.reshape(tf.nn.bias_add(conv, b), tf.shape(conv))
 
     tf.summary.histogram("{}_w".format(name), w)
     tf.summary.histogram("{}_b".format(name), b)
 
     return conv
 
-def fc(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False, name='fc'):
-    # Linear Fully connected layer
-    shape = input_.get_shape().as_list()
+def fc(input_, output_size, scope=None, stddev=0.02,
+       bias_start=0.0, with_w=False, name='fc'):
+    """ Creates a fully connected layer from the input shape to the given output size.
 
+        - i
+
+    """
+    # Linear Fully connected layer
+    print(name)
+    print(input_.get_shape())
     with tf.variable_scope(scope or "FullyConnected"):
-        w = tf.get_variable('w', [shape[1], output_size], tf.float32,
+        w = tf.get_variable('w', [input_.get_shape()[1], output_size], tf.float32,
                     tf.random_normal_initializer(stddev=stddev))
         b = tf.get_variable('b', [output_size],
         initializer=tf.constant_initializer(bias_start))
-        if with_w:
-            return tf.matmul(input_, w) + b, w, b
-        else:
-            return tf.matmul(input_, w) + b
 
-def freeze_graph(output_node_names):
+        return tf.matmul(input_, w) + b
+
+def freeze_graph(output_node_names, model_name):
     """Extract the sub graph defined by the output nodes and convert
     all its variables into constant
     Args:
@@ -79,7 +102,7 @@ def freeze_graph(output_node_names):
         output_node_names: a string, containing all the output node's names,
                             comma separated
     """
-    model_dir = r'C:\Users\andrew\Documents\Root\Repos\CC\AAA\src\models'
+    model_dir = r'C:\Users\andrew\Documents\Root\Repos\CC\AAA\src\models' # TODO: use relative path
     if not tf.gfile.Exists(model_dir):
         raise AssertionError(
             "Export directory doesn't exists. Please specify an export "
@@ -94,7 +117,7 @@ def freeze_graph(output_node_names):
 
     # We precise the file fullname of our freezed graph
     absolute_model_dir = "\\".join(checkpoint.split('\\')[:-1])
-    output_graph = absolute_model_dir + "\\model.pb"
+    output_graph = absolute_model_dir + "\\" + model_name + "-model.pb"
 
     # We clear devices to allow TensorFlow to control on which device it will load operations
     clear_devices = True
@@ -109,17 +132,6 @@ def freeze_graph(output_node_names):
 
         # get graph definition
         gd = sess.graph.as_graph_def()
-
-        # fix batch norm nodes (Currently break when trying to save as constants.)
-        for node in gd.node:
-            if node.op == 'RefSwitch':
-                node.op = 'Switch'
-                for index in range(len(node.input)):
-                    if 'moving_' in node.input[index]:
-                        node.input[index] = node.input[index] + '/read'
-            elif node.op == 'AssignSub':
-                node.op = 'Sub'
-                if 'use_locking' in node.attr: del node.attr['use_locking']
 
         # We use a built-in TF helper to export variables to constants
         output_graph_def = tf.graph_util.convert_variables_to_constants(
