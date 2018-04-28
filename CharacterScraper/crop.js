80253950216cdf53a0f4e462b80fbc63b9dc9473
Andrewmumblebee <aceharris@hotmail.co.uk>
2018-04-28 09:45:59 +0100

    Updated cropping app to use correct settings

--- CharacterScraper/crop.js
+++ CharacterScraper/crop.js
@@ -18,12 +18,16 @@ function cropAnimation(img, startY, frameCount, animCount, spriteId, animId) {
       .write(`./dump/animations/a${animId + y}_${spriteId}b.png`);
     }
 
+    let promises = [];
+
     for (let x = 0; x < frameCount; x++) {
       let x_ = (x + 1);
       for (let y = 0; y < animCount; y++) {
         yPos = y * 64 + startY;
+
         img.clone().crop(x_ * 64, y * 64 + startY, 64, 64)
         .write(`./dump/animations/a${animId + y}_f${x}_${spriteId}.png`);
+
       }
     }
 }
@@ -49,15 +53,15 @@ function cropAnimation(img, startY, frameCount, animCount, spriteId, animId) {
           if (random < 1/6)
             cropAnimation(img, 0, 7, 3, i, 0);
           if (1/6 <= random && random < 1/6 * 2)
-            cropAnimation(img, 64 * 4, 8, 3, i, 3);
+            cropAnimation(img, 64 * 4, 7, 3, i, 3);
           if (1/6 * 2 <= random && random < 1/6 * 3)
-            cropAnimation(img, 64 * 8, 9, 3, i, 6);
+            cropAnimation(img, 64 * 8, 8, 3, i, 6);
           if (1/6 * 3 <= random && random < 1/6 * 4)
-            cropAnimation(img, 64 * 12, 6, 3, i, 9);
+            cropAnimation(img, 64 * 12, 5, 3, i, 9);
           if (1/6 * 4 <= random && random < 1/6 * 5)
-            cropAnimation(img, 64 * 16, 13, 3, i, 12);
+            cropAnimation(img, 64 * 16, 12, 3, i, 12);
           if (1/6 * 5 <= random)
-            cropAnimation(img, 64 * 20, 6, 1, i, 15);
+            cropAnimation(img, 64 * 20, 5, 1, i, 15);
 
           // let n = 7; // Frame count
           // img.clone().crop(0, 0, 64, 64)
