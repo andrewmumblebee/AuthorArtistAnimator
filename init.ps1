262f3a96699787d0489cac31431ef692896e1a98
Andrew <Aceharris@hotmail.co.uk>
2018-04-29 12:44:49 +0100

    Init script now installs requirements

--- init.ps1
+++ init.ps1
@@ -1,7 +1,7 @@
 # Initializes the web site, running the flask server, installing required packages.
 # .\venv\Scripts\Activate.ps1
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 $Env:FLASK_DEBUG=1
 $Env:FLASK_APP= "$($PSScriptRoot)\web\server.py"
